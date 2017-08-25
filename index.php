03e4e6414441e5fe3f287f9a23e4ad3bad3dc086
Joel Casillas <jcasillas@csumb.edu>
2017-08-25 20:21:32 +0000

    adding index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> </title>
+        <meta charset="utf-8"/>
+    </head>
+    <body>
+        
+        <h1> welcome to joel's Webpage</h1>
+
+    </body>
+</html>
