f8bf33f04b5370b4f2726e38c4a373613029495f
Joel Casillas <jcasillas@csumb.edu>
2017-09-11 02:42:25 +0000

    Slot Machine

---
+++ labs/lab2/index.php
@@ -0,0 +1,35 @@
+<?php
+include 'inc/functions.php';
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> 777 Slot Machine
+        </title>
+        
+        <meta charset="utf-8"/>
+        
+    <style>
+        @import url("css/styles.css");
+    </style>
+    </head>
+     
+       <div id="main">
+    <body>
+        
+    <div id="main">
+        
+    <?php
+            play();
+     ?>
+   
+    <form>
+        <input type="submit" value="Spin!"/>
+    </form>
+    </div>
+    
+
+    </body>
+</html>
+
